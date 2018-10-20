280f97a6ee5941610efd32b846e5e1259f227e3a
Lowell Buttorff <lbuttorff@gmail.com>
2018-10-19 20:20:35 -0400

    Add a bunch of builder things

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 
-const buildRouter = require('./build_router');
+const router = require('./router');
 const app = express();
 
 const port = 3000;
@@ -14,15 +14,7 @@ app.use(cookieParser());
 app.use(express.static('public'));
 
 // Set router(s)
-app.use('/app/builder', buildRouter);
-/*app.get('/app/*', (req, res) => {
-    console.info("Serving request to ", req.route.methods, req.route.path);
-    res.render('under_construction');
-});*/
-app.get('/', (req, res) => {
-    console.info("Serving request to ", req.route.methods, req.route.path);
-    res.render('main_view');
-});
+app.use('/', router);
 
 // Set port for the server
 app.listen(port, () => console.info('dbd-apps-ui running on port: ', port));
--- src/router.js
+++ src/router.js
@@ -10,6 +10,14 @@ router.get('/', (req, res) => {
 });
 
 /**
+ * Route to the build maker
+ */
+router.get('/app/builder', (req, res) => {
+    console.info("Serving request to ", req.route.methods, req.route.path);
+    res.render('builder');
+});
+
+/**
  * Router to get pages belonging to the app directory
  */
 router.get('/app/*', (req, res) => {
